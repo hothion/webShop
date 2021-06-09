c90eca01c4a6576f8cb3c064249a01d73de62bc0
anhnguyen21 <anhanh5811@gmail.com>
2021-06-10 00:23:47 +0700

    New ypdate

--- src/main.js
+++ src/main.js
@@ -132,7 +132,7 @@ const dataUser = JSON.parse(localStorage.getItem('data'));
 if(dataUser === 1){
     const router = new VueRouter({mode: 'history', routes: routes});
     new Vue({
-        render: h => h(NavigationAdmin),
+        render: h => h(Navigation),
         router,
         store
     }).$mount('#app')
